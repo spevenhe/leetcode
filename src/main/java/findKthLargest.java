b95d5d9c39e341884c94f49f39cb589fd30f07e8
spevenhe <spevenhe@gmail.com>
2021-09-17 00:06:12 +0800

    K largest number

---
+++ src/main/java/findKthLargest.java
@@ -0,0 +1,55 @@
+public class findKthLargest {
+
+    public static void main(String[] args) {
+
+        int[] a = {3,2,1,5,6,4};
+        int k = 2;
+        System.out.println(findKthLargest(a,k));
+
+    }
+    public static int findKthLargest(int[] nums, int k) {
+        if (nums.length== 0 || k < 1 || k > nums.length){
+            return -1;
+        }
+        int left = 0;
+        int right = nums.length - 1;
+        return findUtil(nums,left,right,nums.length - k);
+
+    }
+
+    public static int findUtil(int nums[],int left,int right,int k){
+        int start = left;
+        int end = right;
+        if(left>=right){
+            return nums[k];
+        }
+        int rand = left;
+        int pivot = nums[rand];
+        while (left <= right){
+            while(left <= right &&  nums[left]<pivot){
+                left++;
+            }
+            while (left <= right &&  nums[right]>pivot){
+                right --;
+            }
+            if(left<=right){
+                swap(nums,left,right);
+                left++;
+                right--;
+            }
+        }
+        if((k)>=left){
+            return findUtil(nums,left,end,k);
+
+        }if((k)<=right) {
+            return findUtil(nums,start,right,k);
+        }
+        return nums[k];
+    }
+
+    public static void swap(int[]nums, int left, int right){
+        int tmp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = tmp;
+    }
+}
